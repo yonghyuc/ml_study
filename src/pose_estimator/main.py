67984c8a28f8abd3279db877d73af4e33596f214
yonghyuc <yonghyuc@usc.edu>
2019-08-06 04:45:47 -0700

    add cat_dog_classifier using Alexnet

---
+++ src/pose_estimator/main.py
@@ -0,0 +1,121 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import pprint
+import shutil
+
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+
+
+
+def reset_config(config, args):
+    if args.gpus:
+        config.GPUS = args.gpus
+    if args.workers:
+        config.WORKERS = args.workers
+
+
+def main():
+    model = eval('models.'+config.MODEL.NAME+'.get_pose_net')(
+        config, is_train=True
+    )
+
+    # copy model file
+    this_dir = os.path.dirname(__file__)
+    shutil.copy2(
+        os.path.join(this_dir, '../lib/models', config.MODEL.NAME + '.py'),
+        final_output_dir)
+
+    writer_dict = {
+        'writer': SummaryWriter(log_dir=tb_log_dir),
+        'train_global_steps': 0,
+        'valid_global_steps': 0,
+    }
+
+    dump_input = torch.rand((config.TRAIN.BATCH_SIZE,
+                             3,
+                             config.MODEL.IMAGE_SIZE[1],
+                             config.MODEL.IMAGE_SIZE[0]))
+    writer_dict['writer'].add_graph(model, (dump_input, ), verbose=False)
+
+    gpus = [int(i) for i in config.GPUS.split(',')]
+    model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
+
+    # define loss function (criterion) and optimizer
+    # Mean Squared Error (MSE)
+    criterion = JointsMSELoss(
+        use_target_weight=config.LOSS.USE_TARGET_WEIGHT
+    ).cuda()
+
+    # adam
+    optimizer = get_optimizer(config, model)
+
+    # Data loading code
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_dataset = eval('dataset.'+config.DATASET.DATASET)(
+        config,
+        config.DATASET.ROOT,
+        config.DATASET.TRAIN_SET,
+        True,
+        transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])
+    )
+    valid_dataset = eval('dataset.'+config.DATASET.DATASET)(
+        config,
+        config.DATASET.ROOT,
+        config.DATASET.TEST_SET,
+        False,
+        transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=config.TRAIN.BATCH_SIZE*len(gpus),
+        shuffle=config.TRAIN.SHUFFLE,
+        num_workers=config.WORKERS,
+        pin_memory=True
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=config.TEST.BATCH_SIZE*len(gpus),
+        shuffle=False,
+        num_workers=config.WORKERS,
+        pin_memory=True
+    )
+
+    best_perf = 0.0
+    best_model = False
+    for epoch in range(config.TRAIN.BEGIN_EPOCH, config.TRAIN.END_EPOCH):
+        # train for one epoch
+        train(config, train_loader, model, criterion, optimizer, epoch,
+              final_output_dir, tb_log_dir, writer_dict)
+
+
+        # evaluate on validation set
+        perf_indicator = validate(config, valid_loader, valid_dataset, model,
+                                  criterion, final_output_dir, tb_log_dir,
+                                  writer_dict)
+
+    final_model_state_file = os.path.join(final_output_dir, 'final_state.pth.tar')
+
+    torch.save(model.module.state_dict(), final_model_state_file)
+
+
+if __name__ == '__main__':
+    main()
